d67dae54d63da1bf11674b7fc3ec713a87f49a80
edirimkus <93425953+edirimkus@users.noreply.github.com>
2024-10-13 12:45:07 +0300

    Add files via upload

---
+++ restart all pcs that starts PS1111 and PS2222 based on active directory.ps1
@@ -0,0 +1,3 @@
+﻿Import-Module ActiveDirectory
+$computers = Get-ADComputer -Filter {Name -like "PS1111*" -or Name -like "PS2222*"} |Select-Object -ExpandProperty Name
+Restart-Computer -ComputerName $computers -Force -AsJob
